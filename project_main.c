eda8e343256e1bc3eb69a2685d01fb4e93f95d72
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-27 13:24:18 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,1 +1,32 @@
 
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    DDRD&=~(4<<PD0);
+    PORTD|=(1<<PD0);
+    PORTD|=(4<<PD0);
+
+    while(1)
+{
+    if(!(PIND&(1<<PD0)) && !(PIND&(4<<PD0)))
+    {
+        PORTB|=(1<<PB0);
+    _delay_ms(200);
+    }
+    else
+        {
+
+    PORTB&=~(1<<PB0);
+    _delay_ms(200);
+    }
+
+}
+
+    return 0;
+}
