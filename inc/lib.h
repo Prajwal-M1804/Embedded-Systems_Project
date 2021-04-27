a8a5317e79faf7756324818731456bf71ff24e20
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-27 16:05:19 +0530

    Create lib.h

---
+++ inc/lib.h
@@ -0,0 +1,15 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+
+/**
+ * @file lib.h
+ * @author Prajwal M
+ * @brief User defined utilities used across the project
+ * 
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#endif /* __LIB_H__ */
