701c52b6427484a6a434915a9b8ff9353cdd7d56
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-29 16:32:50 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -9,12 +9,7 @@
 
 int main(void)
 {
-    DDRB|=(1<<PB0);
-    DDRD&=~(1<<PD0);
-    DDRD&=~(4<<PD0);
-    PORTD|=(1<<PD0);
-    PORTD|=(4<<PD0);
-
+    activity1();
     while(1)
 {
     if(!(PIND&(1<<PD0)) && !(PIND&(4<<PD0)))
@@ -33,3 +28,4 @@ int main(void)
 
     return 0;
 }
+
