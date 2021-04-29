ab01028b0ff6fab022b788163f075e5b5a4ef213
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-29 23:57:14 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -7,6 +7,7 @@
 
 #include "activity1.h"
 #include "activity2.h"
+#include "activity3.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -15,6 +16,10 @@ int main(void)
     activity1();
     InitADC();
     uint16_t temp;
+    void InitPWM();
+    char OutPWM ();
+    uint16_t temp_data;
+    
     while(1)
 {
     if(!(PIND&(1<<PD0)) && !(PIND&(4<<PD0)))
