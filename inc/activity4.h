87280080ee17192d3a7302f71af514b68b059fe8
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-30 02:47:21 +0530

    Create activity4.h

---
+++ inc/activity4.h
@@ -0,0 +1,14 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
+
+#include<avr/io.h>
+
+void USARTInit(uint16_t );
+
+
+
+void USARTwrite(char data);
+
+
+
+#endif // ACTIVITY4_H_INCLUDED
