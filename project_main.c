5ee66130cfc9827446c2027cb20568ba30594429
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-29 18:41:41 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -6,17 +6,23 @@
 
 
 #include "activity1.h"
+#include "activity2.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
     activity1();
+    InitADC();
+    uint16_t temp;
     while(1)
 {
     if(!(PIND&(1<<PD0)) && !(PIND&(4<<PD0)))
     {
         PORTB|=(1<<PB0);
+    _delay_ms(200);
+        
+        temp=ReadADC(0);
     _delay_ms(200);
     }
     else
