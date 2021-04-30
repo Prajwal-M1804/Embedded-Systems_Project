ff32ea7e31b9e8412fc1cbfc6cb5645eab985c16
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-30 14:40:24 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,48 +1,27 @@
-#include"activity3.h"
-#include<util/delay.h>
+
 #include<avr/io.h>
+#include"activity3.h"
 
-void InitPWM(void)
+void init_PWM()
 {
-    /*Configuring the registers and ports*/
-    TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
-    TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);
-    DDRB|=(1<<PB1);
-
+    TCCR0A |= (1<<COM0A1)|(1<<WGM01)|(1<<WGM00);
+    TCCR0B |= (1<<CS00);
+    DDRD |= (1<<PD6);
 }
 
-char OutPWM (uint16_t ADC_val)
+
+void compare_adc()
 {
-
-    char temperature;
-
-    if((ADC_val>=0) && (ADC_val<=209)){
-
-        OCR1A = 205; //20% duty cycle
-        temperature = 20;
-        _delay_ms(20);
+     if (ADC>0 && ADC<209){
+        OCR0A = 51;
     }
-    else if((ADC_val>=210) && (ADC_val<=509)){
-
-        OCR1A = 410; //40% duty cycle
-        temperature = 25;
-       _delay_ms(20);
+    else if(ADC > 210 && ADC < 509){
+        OCR0A = 101;
     }
-    else if((ADC_val>=510) && (ADC_val<=709)){
-
-        OCR1A = 717;//70% duty cycle
-        temperature = 29;
-        _delay_ms(20);
+    else if(ADC > 510 && ADC < 709){
+        OCR0A = 178;
     }
-    else if((ADC_val>=710) && (ADC_val<=1024)){
-
-        OCR1A = 973; //95% duty cycle
-        temperature = 33;
-        _delay_ms(20);
+    else if (ADC > 710 && ADC < 1024){
+        OCR0A = 242;
     }
-    else{
-        OCR1A = 0; //0% output
-        temperature = 0;
-    }
-    return temperature;
 }
