37200ba3cfa737135745854375a5abfb1d3b40e0
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-30 14:42:30 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -5,22 +5,29 @@
  */
 
 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
 #include "activity1.h"
 #include "activity2.h"
 #include "activity3.h"
 #include "activity4.h"
 
-#include <avr/io.h>
-#include <util/delay.h>
+
 
 int main(void)
 {
     activity1();
+    
     InitADC();
     uint16_t temp;
-    void InitPWM();
-    char OutPWM ();
-    char temp_data;
+    
+   init_PWM();
+   compare_adc();
+   OCR0A = temp;
+   _delay_ms(200);
+    
     init_uart(103);
     
     while(1)
@@ -33,7 +40,10 @@ int main(void)
         temp=ReadADC(0);
     _delay_ms(200);
         
-        temp_data = OutPWM(temp);
+       compare_adc();
+    OCR0A = temp;
+    _delay_ms(200);
+
         
         USARTWriteChar(temp);
     }
@@ -43,7 +53,7 @@ int main(void)
 
     PORTB&=~(1<<PB0);
     _delay_ms(200);
-        OCR1A=0;
+        
     }
 
 }
