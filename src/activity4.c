fa74c56e6f9024bc9ba68b62c8a16781b04346c4
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-04-30 13:25:40 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -1,28 +1,75 @@
 #include<avr/io.h>
 #include"activity4.h"
 
-void USARTInit(uint16_t ubrr_value)
+void init_uart(uint16_t ubrr_value)
 {
-    // set Baud rate
-
     UBRR0L = ubrr_value;
-    UBRR0H = (ubrr_value>>8)&(0x00ff);
+    UBRR0H = (ubrr_value>>8)&0x00ff;
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
-
-    // Enable the receiver and transmitter
-
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+}
 
+char USARTReadChar()
+{
+    while(!(UCSR0A & (1<<RXC0)))
+    {
+      //do nothing
+    }
+    return UDR0;
 }
 
 
-void USARTwrite(char data)
+void USARTWriteChar(uint16_t temp_value)
 {
-    // wait until the transmitter is ready
-    while(!(UCSR0A & (1<<UDRE0)))
+
+    if(temp_value>=0&&temp_value<=209)
     {
-        //do nothing
+        unsigned char output01[]="Temperature is 20 degree C \n";
+        int i = 0;
+        while (output01[i]!=0)
+        {
+        while(!(UCSR0A&(1<<UDRE0)));
+        UDR0=output01[i];
+        i++;
+        }
     }
 
-    UDR0 = data;
+    if(temp_value>=210&&temp_value<=509)
+    {
+        unsigned char output02[]="Temperature is 25 degree C \n";
+        int i = 0;
+        while (output02[i]!=0)
+        {
+        while(!(UCSR0A&(1<<UDRE0)));
+        UDR0=output02[i];
+        i++;
+        }
+    }
+
+    if(temp_value>=510&&temp_value<=709)
+    {
+        unsigned char output03[]="Temperature is 29 degree C \n";
+        int i = 0;
+        while (output03[i]!=0)
+        {
+        while(!(UCSR0A&(1<<UDRE0)));
+        UDR0=output03[i];
+        i++;
+        }
+    }
+
+
+    if(temp_value>=710&&temp_value<=1024)
+    {
+        unsigned char output04[]="Temperature is 33 degree C \n";
+        int i = 0;
+        while (output04[i]!=0)
+        {
+        while(!(UCSR0A&(1<<UDRE0)));
+        UDR0=output04[i];
+        i++;
+        }
+    }
+
 }
+
